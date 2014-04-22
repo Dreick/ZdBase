fe252917b112954bfd0ec0b6711fa1c2adec9a9d
Dreick <prizzrak_13@mail.ru>
2014-04-22 23:27:42 +0600

    Create Module.php

---
+++ Module.php
@@ -0,0 +1,21 @@
+<?php
+namespace ZdBase;
+
+use Zend\ModuleManager\Feature;
+
+class Module implements Feature\AutoloaderProviderInterface
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+}
