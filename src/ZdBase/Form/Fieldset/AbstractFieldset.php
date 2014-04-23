59d6568de3caa85e3bdae4a929ed948e0df48d83
Dreick <prizzrak_13@mail.ru>
2014-04-23 22:04:06 +0600

    Create AbstractFieldset.php

---
+++ src/ZdBase/Form/Fieldset/AbstractFieldset.php
@@ -0,0 +1,50 @@
+<?php
+namespace ZdBase\Form\Fieldset;
+
+use Zend\Form\Fieldset;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use DoctrineModule\Stdlib\Hydrator\DoctrineObject as DoctrineHydrator;
+
+class AbstractFieldset extends Fieldset implements ServiceLocatorAwareInterface
+{
+    protected $serviceLocator;
+    
+    protected $entityManager;
+    
+    public function setDoctrineHydrator($classEntity)
+    {
+        $this->setHydrator(new DoctrineHydrator($this->getEntityManager(), $classEntity));
+        
+        return $this;
+    }
+    
+    public function setServiceLocator(ServiceLocatorInterface $sl)
+    {
+        $this->serviceLocator = $sl->getServiceLocator();
+        
+        return $this;
+    }
+
+    public function getServiceLocator()
+    {
+        return $this->serviceLocator;
+    }
+    
+    public function setEntityManager(ObjectManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+ 
+        return $this;
+    }
+ 
+    public function getEntityManager()
+    {
+        if(!$this->entityManager instanceof ObjectManager) {
+            $this->setEntityManager($this->getServiceLocator()->get('zdbase_doctrine_em'));
+        }
+        
+        return $this->entityManager;
+    }
+}
