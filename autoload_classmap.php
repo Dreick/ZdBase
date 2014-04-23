c6b61eb6b1607c8c1e1c3d940d0673b859e73748
Dreick <prizzrak_13@mail.ru>
2014-04-23 23:22:12 +0600

    Update autoload_classmap.php

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -6,4 +6,5 @@ return array(
   'ZdBase\Module\AbstractModule'             => __DIR__ . '/src/ZdBase/Module/AbstractModule.php',
   'ZdBase\Validator\Doctrine\NoObjectExists' => __DIR__ . '/src/ZdBase/Validator/Doctrine/NoObjectExists.php',
   'ZdBase\Validator\Doctrine\ObjectExists'   => __DIR__ . '/src/ZdBase/Validator/Doctrine/ObjectExists.php',
+  'ZdBase\View\Helper\getRoute'              => __DIR__ . '/src/ZdBase/View/Helper/getRoute.php',
 );
