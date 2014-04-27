8f6616b669e5c56e9033eec9a1c663c8b2bb9b7d
Dreick <prizzrak_13@mail.ru>
2014-04-27 15:33:19 +0600

    Update Module.php

--- Module.php
+++ Module.php
@@ -2,11 +2,19 @@
 namespace ZdBase;
 
 use Zend\ModuleManager\Feature;
+use Doctrine\Common\Annotations\AnnotationRegistry;
 
 class Module implements
     Feature\AutoloaderProviderInterface,
     Feature\ServiceProviderInterface
 {
+    public function init()
+    {
+        $namespace = 'Gedmo\Mapping\Annotation';
+        $lib       = 'vendor/gedmo/doctrine-extensions/lib';
+        
+        AnnotationRegistry::registerAutoloadNamespace($namespace, $lib);
+    }
     public function getAutoloaderConfig()
     {
         return array(
