8007502d9da824f21a1187a0cee34d8c90e7ce58
Dreick <prizzrak_13@mail.ru>
2014-05-15 08:06:38 +0600

    Create AbstractHelper.php

---
+++ src/ZdBase/View/Helper/AbstractHelper.php
@@ -0,0 +1,36 @@
+<?php
+namespace ZdBase\View\Helper;
+
+use Zend\View\Helper\AbstractHelper as Helper;
+use Zend\ServiceManager\ServiceManager;
+
+class AbstractHelper extends Helper
+{
+    protected $serviceManager;
+    
+    /**
+     * Retrieve service manager instance
+     *
+     * @return ServiceManager
+     */
+    public function getServiceManager()
+    {
+        if(!$this->serviceManager instanceof ServiceManager) {
+            $this->setServiceManager($this->getView()->getHelperPluginManager()->getServiceLocator());
+        }
+        
+        return $this->serviceManager;
+    }
+
+    /**
+     * Set service manager instance
+     *
+     * @param ServiceManager $serviceManager
+     * @return BreadCrumbsWidget
+     */
+    public function setServiceManager(ServiceManager $serviceManager)
+    {
+        $this->serviceManager = $serviceManager;
+        return $this;
+    }
+}
